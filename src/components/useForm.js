913f3a2c3ffc95ba0368e74cca9c7335d76dd3a0
guimariz <guimariz@gmail.com>
2021-02-09 07:16:33 -0300

    Axios com problemas

--- src/components/useForm.js
+++ src/components/useForm.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios-hooks';
 
 const useForm = (callback, validate) => {
   const [values, setValues] = useState({
@@ -10,10 +11,15 @@ const useForm = (callback, validate) => {
     checkbox3: false,
   });
 
+  const token =
+    '389a35ce910852e1d98eef5589e4b5c239378b38171973fb6ee2fca59495667b';
+  const key = '803b3f5cbb8e033ce69e114c370cad70';
+  const idList = '601eebb5e1ad525657d47a0a';
+
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [dropdown, setDropDown] = useState(0);
-  const [data, setData] = useState({});
+  const [cards, setCards] = useState({ id: 0, name: '', desc: '' });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -38,19 +44,27 @@ const useForm = (callback, validate) => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    let data = {
+    setErrors(validate(values));
+    console.log(cards);
+    setCards({
       id: 1,
       name: values.userName,
-      email: values.email,
-      description: values.textArea,
-      checkbox1: values.checkbox1,
-      checkbox2: values.checkbox2,
-      checkbox3: values.checkbox3,
-      dropdown: dropdown,
-      tag: 1,
-    };
+      desc: `E-mail: ${values.email}\n Descrição: ${values.textArea}\n Checkbox1: ${values.checkbox1}\n Checkbox2: ${values.checkbox2}\n Checkbox3: ${values.checkbox3}\n Dropdown: Opção 2\n tag: 1`,
+    });
+    console.log(cards);
 
-    setErrors(validate(values));
+    axios
+      .post(
+        `https://api.trello.com/1/cards?key=${key}&token=${token}&idList=${idList}&name=${cards.name}&desc=${cards.desc}`,
+        this.state
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
     setIsSubmitting(true);
   };
 
